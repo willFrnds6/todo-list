96e3ddb380a1d8c8752449f64928c5133a951631
William Fernandes <williamdu63800@gmail.com>
2021-01-20 17:03:24 +0100

    Add checkbox, new functions ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  
+  public todoArray : Array<string> = [
+    'items 1',
+    'items 2',
+    'items 3',
+    'items 4'
+  ];
+
+  //void = bloc le return
+  public addItem($event : KeyboardEvent) : void {
+    const $input: HTMLInputElement|null = $event.target as HTMLInputElement;
+
+    if($event.code === 'Enter'){
+      console.log($event);
+    }
+    
+  }
 }

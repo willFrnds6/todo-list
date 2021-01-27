4128ef60b6fbdcb7ff4b13f3a1e28984d31a0aa6
William Fernandes <williamdu63800@gmail.com>
2021-01-27 10:37:21 +0100

    Creation of static element

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -6,18 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent implements OnInit {
+  // Create a static element. This element will be execute only one time
+  private static counter = 0; 
   public id : string;
 
   constructor() {
-    // Generate a random nomber beteween 0 and 1 000 000 for id checkbox
-    let rand = Math.random();
-    rand = rand * 1000000;
-    Math.round(rand);
+    /* 1er methode
+      // Generate a random nomber beteween 0 and 1 000 000 for id checkbox
+      let rand = Math.random();
+      rand = rand * 1000000;
+      Math.round(rand);
 
-    this.id = 'app-checkbox-' + rand;
+      this.id = 'app-checkbox-' + rand;
+    */
+
+    // Static methode
+    this.id = 'app-checkbox-' + CheckboxComponent.counter;
+    CheckboxComponent.counter++;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }

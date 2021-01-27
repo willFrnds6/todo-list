bad5ffc91c93d42cbcc2f70743429bb2d18c1571
William Fernandes <williamdu63800@gmail.com>
2021-01-27 09:32:45 +0100

    Add checkbox component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckboxComponent
   ],
   imports: [
     BrowserModule,
